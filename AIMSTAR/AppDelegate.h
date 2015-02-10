a1574e49c3f452ccdaf5540b363a1aef00ad500c
rnut <rnut.ist@gmail.com>
2015-02-10 16:35:28 +0700

    create project
    1st day work

---
+++ AIMSTAR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AIMSTAR
+//
+//  Created by Rnut on 2/10/2558 BE.
+//  Copyright (c) 2558 Rnut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
